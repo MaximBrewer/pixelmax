540f5da9b18827b800061a6268486a0c19b253f2
Максим Пивоваров <pimax1978@icloud.com>
2020-02-02 00:15:29 +0600

    All

--- resources/lang/ru/auth.php
+++ resources/lang/ru/auth.php
@@ -15,22 +15,4 @@ return [
 
     'failed' => 'Неверные учетные данные.',
     'throttle' => 'Слишком много попыток входа в систему. Пожалуйста, повторите попытку через :seconds секунд(у/ы).',
-
-	"Verify Your Email Address": "Подтвердите адрес электронной почты",
-	"Before proceeding, please check your email for a verification link. If you did not receive the email,": "Прежде чем продолжить, проверьте свою электронную почту на наличие ссылки для подтверждения. Если вы не получили письмо,",
-	"click here to request another": " нажмите здесь, чтобы повторить",
-	"If you’re having trouble clicking the \"Verify Email Address\" button, copy and paste the URL below into your web browser": "Если у вас возникли проблемы с нажатием кнопки «Подтвердить адрес электронной почты», скопируйте и вставьте приведенный ниже URL-адрес в веб-браузер.",
-	"Hello!": "Здравтсвуйте",
-	"Please click the button below to verify your email address.": "Пожалуйста, нажмите кнопку ниже, чтобы подтвердить свой адрес электронной почты.",
-	"Verify Email Address": "Подтвердить",
-	"If you did not create an account, no further action is required.": "Если вы не создавали учетную запись, никаких дальнейших действий не требуется.",
-	"A fresh verification link has been sent to your email address.": "На ваш адрес электронной почты была отправлена ​​новая ссылка для подтверждения.",
-	"Before proceeding, please check your email for a verification link.\nIf you did not receive the email,": "Прежде чем продолжить, проверьте свою электронную почту на наличие ссылки для подтверждения.\nЕсли вы не получили письмо,",
-	"Before proceeding, please check your email for a verification link.": "Прежде чем продолжить, проверьте свою электронную почту на наличие ссылки для подтверждения.",
-	"If you did not receive the email,": "Если вы не получили письмо,",
-	"If you did not receive the email, ": "Если вы не получили письмо, ",
-	"If you did not receive the email": "Если вы не получили письмо",
-	"Regards": "С Уважением",
-	"Regards, ": "С Уважением, ",
-	"Regards,": "С Уважением,",
 ];
